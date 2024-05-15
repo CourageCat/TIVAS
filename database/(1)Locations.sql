fc2cdb815465848021437fb2643d049121697884
CourageCat <minhhien270103@gmail.com>
2024-05-15 12:57:38 +0700

    Change name for sql files of database

---
+++ database/(1)Locations.sql
@@ -0,0 +1,14 @@
+INSERT INTO `Locations` (`id`, `name`, `imagePathUrl`, `imagePathName`, `createdAt`, `updatedAt`) VALUES
+(1, 'Ninh Thuan', 'https://res.cloudinary.com/tivas/image/upload/v1709724652/tivas_image/Ninh_Thuan.jpg', 'Ninh_Thuan', '2024-03-06 11:26:56', '2024-03-06 11:26:56');
+INSERT INTO `Locations` (`id`, `name`, `imagePathUrl`, `imagePathName`, `createdAt`, `updatedAt`) VALUES
+(2, 'Ben Tre', 'https://res.cloudinary.com/tivas/image/upload/v1709724829/tivas_image/Ben_Tre.jpg', 'Ben_Tre', '2024-03-06 11:26:56', '2024-03-06 11:26:56');
+INSERT INTO `Locations` (`id`, `name`, `imagePathUrl`, `imagePathName`, `createdAt`, `updatedAt`) VALUES
+(3, 'Vung Tau', 'https://res.cloudinary.com/tivas/image/upload/v1709724861/tivas_image/Vung_Tau.jpg', 'Vung_Tau', '2024-03-06 11:26:56', '2024-03-06 11:26:56');
+INSERT INTO `Locations` (`id`, `name`, `imagePathUrl`, `imagePathName`, `createdAt`, `updatedAt`) VALUES
+(4, 'Da Lat', 'https://res.cloudinary.com/tivas/image/upload/v1709724888/tivas_image/Da_Lat.jpg', 'Da_Lat', '2024-03-06 11:26:56', '2024-03-06 11:26:56'),
+(5, 'Nha Trang', 'https://res.cloudinary.com/tivas/image/upload/v1709724928/tivas_image/Nha_Trang.png', 'Nha_Trang', '2024-03-06 11:26:56', '2024-03-06 11:26:56'),
+(6, 'Ho Chi Minh City', 'https://res.cloudinary.com/tivas/image/upload/v1710143897/tivas_image/Ho_Chi_Minh_City.jpg', 'Ho_Chi_Minh_City', '2024-03-11 07:52:17', '2024-03-11 07:52:17'),
+(7, 'Ha Noi', 'https://res.cloudinary.com/tivas/image/upload/v1710144058/tivas_image/Ha_Noi.jpg', 'Ha_Noi', '2024-03-11 07:52:17', '2024-03-11 07:52:17'),
+(8, 'Da Nang', 'https://res.cloudinary.com/tivas/image/upload/v1710144125/tivas_image/Da_Nang.jpg', 'Da_Nang', '2024-03-11 07:52:17', '2024-03-11 07:52:17'),
+(9, 'Hoi An', 'https://res.cloudinary.com/tivas/image/upload/v1710144167/tivas_image/Hoi_An.jpg', 'Hoi_An', '2024-03-11 07:52:17', '2024-03-11 07:52:17'),
+(10, 'Hue', 'https://res.cloudinary.com/tivas/image/upload/v1710144199/tivas_image/Hue.jpg', 'Hue', '2024-03-11 07:52:17', '2024-03-11 07:52:17');
